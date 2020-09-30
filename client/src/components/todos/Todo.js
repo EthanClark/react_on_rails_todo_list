a23d9a2ad1d96737db0de6800d24fee162507e55
Ethan Clark <clark.ethan.r@gmail.com>
2020-09-29 18:12:18 -0600

    created components

---
+++ client/src/components/todos/Todo.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Header, Modal, Button } from 'semantic-ui-react';
+import TodoForm from './TodoForm';
+class Todo extends Component {
+  state = { editing: false }
+  open = () => this.setState({ editing: true })
+  close = () => this.setState({ editing: false })
+  render() {
+    const { id, title, complete, updateTodo, deleteTodo } = this.props 
+    const { editing } = this.state 
+    return(
+      <div>
+        <Header style={ complete ? styles.complete : {} }>
+          {title}
+        </Header>
+        <Modal
+          onOpen={() => this.open()}
+          open={editing}
+          trigger={<Button>Edit</Button>}
+        >
+          <Modal.Content>
+            <TodoForm 
+              updateTodo={updateTodo}
+              close={this.close}
+              id={id}
+              title={title}
+              complete={complete}
+            />
+          </Modal.Content>
+        </Modal>
+        <Button color='red' onClick={() => deleteTodo(id)}>Delete</Button>
+      </div>
+    )
+  }
+}
+const styles = { 
+  complete: {
+    textDecoration: 'line-through',
+    color: 'grey'
+  }
+}
+export default Todo;
---
+++ client/src/components/todos/TodoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Todo from './Todo';
+const TodoList = ({ todos, updateTodo, deleteTodo }) => (
+  <div>
+    {
+      todos.map( t => 
+        <Todo 
+          key={t.id}
+          {...t}
+          updateTodo={updateTodo}
+          deleteTodo={deleteTodo}
+        />
+      )
+    }
+  </div>
+)
+export default TodoList;
